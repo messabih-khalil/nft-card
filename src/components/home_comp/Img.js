6461a7db25d2f32c93aa0fec399aad8260ca655f
messabih-khalil <msbih.khalil@gmail.com>
2023-04-29 18:10:46 +0100

    feat(Img.js): add Image component to display image in home page

---
+++ src/components/home_comp/Img.js
@@ -0,0 +1,9 @@
+import Img from '../../static/images/image-equilibrium.jpg';
+
+export const Image = () => {
+  return (
+    <div className="image-container">
+      <img src={Img} alt="" />
+    </div>
+  );
+};
