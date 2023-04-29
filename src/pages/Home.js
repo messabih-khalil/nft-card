598da62c558e2e65229b0593c9c6b42086a65451
messabih-khalil <msbih.khalil@gmail.com>
2023-04-29 18:10:59 +0100

    feat(Home.js): add Home page component with Card component inside

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import { Card } from '../components/home_comp/Card';
+import '../static/css/home.css';
+
+export const Home = () => {
+  return (
+    <>
+      <div className="home">
+        <Card></Card>
+      </div>
+    </>
+  );
+};
